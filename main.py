55b4a3aae60c39fc6a1f52ff9b5d3cd910d40234
mattrock12344-collab <mattrock12344@gmail.com>
2025-11-05 10:01:10 -0500

    4: shit I forgot to update uuuh player is triangle and movesgit add .!

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pygame
+from player import Player
 from constants import *
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
 import pygame
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,7 +24,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+        dt = pygame.time.Clock().tick(60) / 1000
 
 
 if __name__ == "__main__":
